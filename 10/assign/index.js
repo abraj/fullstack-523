8d0849babe08283b3497e193649ec7a841f964c0
Abhishek Raj <abhi@raj.me>
2023-06-26 00:57:39 +0530

    Add assignment

---
+++ 10/assign/index.js
@@ -0,0 +1,27 @@
+function printDiamond() {
+  // your code here..
+}
+
+printDiamond(5);
+
+/*
+printDiamond(5) Output: 
+
+  *
+ ***
+*****
+ ***
+  *
+
+*/
+
+printDiamond(3);
+
+/*
+printDiamond(3) Output: 
+
+ *
+***
+ *
+
+*/
