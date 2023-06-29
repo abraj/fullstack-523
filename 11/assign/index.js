fd88c832bf04bb84bfb66e6875f69ee946a7d859
Abhishek Raj <abhi@raj.me>
2023-06-29 18:04:10 +0530

    Assign 08

---
+++ 11/assign/index.js
@@ -0,0 +1,33 @@
+/*******************************/
+// Problem 1:
+
+function firstFibonacci(n) {
+
+}
+
+// firstFibonacci(10):
+// 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+
+/*******************************/
+// Problem 2:
+
+function lastFibonacci(num, k) {
+
+}
+
+// lastFibonacci(500, 5):
+// 55, 89, 144, 233, 377
+
+// lastFibonacci(377, 3):
+// 144, 233, 377
+
+/*******************************/
+// Problem 3:
+
+function sumFibonacci(num, k) {
+
+}
+
+// sumFibonacci(100, 3):
+// returns 754
+// Hint: 144 + 233 + 377 = 754
