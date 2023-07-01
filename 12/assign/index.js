09e838ee999ede20fa85ef909c921cc8295eb842
Abhishek Raj <abhi@raj.me>
2023-07-02 03:47:19 +0530

    Deploy 13

---
+++ 12/assign/index.js
@@ -0,0 +1,30 @@
+/*******************************/
+// Problem 1:
+
+function divBy3List(arr) {
+
+}
+
+// divBy3List([2, 7, 9, 16, 18, 3, 5, 21]):
+// 9, 18, 3, 21
+
+/*******************************/
+// Problem 2:
+
+function getStringList(arr) {
+
+}
+
+// getStringList(['foo', 5, 'bar', true, null, {}, 12.6]):
+// > returns ['foo', 'bar']
+
+/*******************************/
+// Problem 3:
+
+function getRepeatedKeys(arr) {
+
+}
+
+// const arr = [{a: 5}, {b: '5'}, {c: 'foo'}, {a: 5}, {b: 5}, {c: 10}, {d: 16.2}]
+// getRepeatedKeys(arr):
+// > returns ['b', 'c']
