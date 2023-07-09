c569f2b3416b7814cc52f857d786576665dbf5ca
Abhishek Raj <abhi@raj.me>
2023-07-09 18:41:35 +0530

    Deploy 15

---
+++ 14/assign/index.js
@@ -0,0 +1,47 @@
+/*******************************/
+// Problem 1:
+
+function rotateArray(arr, k) {
+
+}
+
+// let arr = [5, 7, 19, 4, 28, 16, 3];
+// rotateArray(arr, 3);
+// console.log(arr);
+// > [4, 28, 16, 3, 5, 7, 19]
+
+/*******************************/
+// Problem 2:
+
+function rotateArray2(arr, k) {
+
+}
+
+// let arr = [5, 7, 19, 4, 28, 16, 3];
+// let arr2 = rotateArray2(arr, 2);
+// console.log(arr2);
+// > [16, 3, 5, 7, 19, 4, 28]
+// console.log(arr);
+// > [5, 7, 19, 4, 28, 16, 3]
+
+/*******************************/
+// Problem 3:
+
+function evenOddMarker(arr) {
+
+}
+
+// let arr = [6, true, 7, 19, 'foo', 0, -4, 23, {}];
+// console.log(evenOddMarker(arr));
+// > ['6:even', '7:odd', '19:odd', '0:even', '-4:even', '23:odd']
+
+/*******************************/
+// Problem 4:
+
+function evenSum(arr) {
+
+}
+
+// let arr = [6, 7, 14, 'foo', 4, 23];
+// console.log(evenSum(arr));
+// > 24
---
+++ 15/readme.md
@@ -0,0 +1,10 @@
+# Class 15: JavaScript Basics
+
+## Required Resources (Pre-requisites)
+* Array methods 3: https://youtu.be/HFh5ykojJO8
+
+## Recommended Resources (Optional)
+
+## Syllabus
+
+## Assignment
