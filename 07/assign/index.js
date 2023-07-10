3c3ffb337696d3bd53cc04dd13e21c5e85717381
Abhishek Raj <abhi@raj.me>
2023-07-10 13:46:36 +0530

    Assign 6.1

---
+++ 07/assign/index.js
@@ -0,0 +1,176 @@
+// Assignment 6.1: Variables and Operators
+
+/*
+// Problem 1
+
+// Complete the following line using normal string (non-template literal string)
+const str1 = ... // TODO
+console.log(str1);
+// hello
+// world
+
+*/
+
+/*
+// Problem 2
+
+// Complete the following line using template literal string (using newline chanracter)
+const str2 = ... // TODO
+console.log(str2);
+// hello
+// world
+
+*/
+
+/*
+// Problem 3
+
+// Complete the following line using template literal string (without using newline chanracter)
+const str3 = ... // TODO
+console.log(str3);
+// hello
+// world
+
+*/
+
+/*
+// Problem 4
+
+// Complete the following line
+const str4 = ... // TODO
+console.log(str4);
+// hello\world
+
+*/
+
+/*
+// Problem 5
+
+// Complete the following line using single-quote string
+const str5 = ... // TODO
+console.log(str5);
+// Fermat's Last Theorem
+
+*/
+
+/*
+// Problem 6
+
+// Complete the following line using double-quote string
+const str6 = ... // TODO
+console.log(str6);
+// Eastern monarch said: "This too shall pass".
+
+*/
+
+/*
+// Problem 7
+// Print integer part and fractional (decimal) part of a number
+
+const a = 5.234;
+const int = ... // TODO
+const frac = ... // TODO
+console.log(int);
+console.log(frac);
+// 5
+// 0.234
+
+*/
+
+/*
+// Problem 8
+// Round a number n to k decimals
+
+const n = 5.2347963;
+const k = 3;
+const r = ... // TODO
+console.log(r);
+// 5.235
+
+*/
+
+/*
+// Problem 9
+// Fill in the blanks (with a, b, c, d only) such that all printed values are `true`
+
+const a = 5
+const b = typeof typeof a;
+const c = typeof a;
+const d = 'number' === ... // TODO
+console.log(d);
+console.log('string' === ...); // TODO
+console.log('boolean' === typeof ...); // TODO
+// true
+// true
+// true
+
+*/
+
+/*
+// Problem 10
+// Fill in the blank such that the printed value is `true`
+
+let a;
+const b = typeof a;
+const c = b === a + ... // TODO
+console.log(c);
+// true
+
+*/
+
+/*
+// Problem 11
+// Fill in the blanks such that executing the following code does not give any error, and prints the string `world`.
+
+const x = 'hello';
+... // TODO
+let x = 'world';
+console.log(x);
+... // TODO
+
+*/
+
+/*
+// Problem 12
+// Replace `func` with an in-built JavaScript function, so that we get the expected output shown below.
+
+let a = 5 / 3;
+let b = a - func(a); // TODO
+console.log(b);
+// 0.6666666666666667
+
+*/
+
+/*
+// Problem 13
+// Replace each `...` with a JavaScript operator, such that we get the expected output shown below.
+
+const num = 25;
+const rem = num ... 7; // TODO
+const quotient = (num - rem) ... 7; // TODO
+console.log(quotient);
+// 3
+
+*/
+
+/*
+// Problem 14
+// Fill in the blanks (`...`) such that executing the following code calculates and prints the cubic root of 64.
+// Ref: https://www.khanacademy.org/math/cc-eighth-grade-math/cc-8th-numbers-operations/cc-8th-roots/a/cube-roots-review
+
+const n = 64;
+const b = ... // TODO
+const c = n ... b; // TODO
+console.log(c);
+
+*/
+
+/*
+// Problem 15
+// Fill in the blank (`...`) such that executing the following code 
+// calculates and prints the value of `1/8` (i.e. `0.125`).
+
+console.log(2 ** ...); // TODO
+// 0.125
+
+*/
