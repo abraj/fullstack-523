a75a488caa70230e0b472cb94ed92d1325134dba
Abhishek Raj <abhi@raj.me>
2023-07-14 01:49:51 +0530

    Deploy 16

---
+++ 15/assign/index.js
@@ -0,0 +1,55 @@
+/*******************************/
+// Problem 1:
+
+function reduceOdd(arr) {
+
+}
+
+// const arr = [5, 7, 19, 4, 28, 16, 3];
+// const arr2 = reduceOdd(arr);
+// console.log(arr2);
+// > [5, 7, 19, 3]
+
+/*******************************/
+// Problem 2:
+
+function flatDeep(arr) {
+
+}
+
+// const arr = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+// const arr2 = flatDeep(arr);
+// console.log(arr2);
+// > [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+/*******************************/
+// Problem 3:
+
+function anyAllPair(arr) {
+
+}
+
+// const arr = [5, 7, 16, 3];
+// const [a, b] = anyAllPair(arr, 7, 3, 5);
+// console.log(a, b)
+// > true true
+// const [c, d] = anyAllPair(arr, 8, 7, 3, 45);
+// console.log(c, d)
+// > true false
+// const [e, f] = anyAllPair(arr, 8, 17, 33, 45, 9);
+// console.log(e, f)
+// > false false
+
+/*******************************/
+// Problem 4:
+
+function objectKeys(arr) {
+
+}
+
+// const arr = [{ a: 5, b: 9, c: 15 }, 5, 19, null, { foo: 'bar' }, 'hello', {}];
+// console.log(objectKeys(arr));
+// > 3 objects, 4 keys
+// const arr2 = [5, {}, 19, []];
+// console.log(objectKeys(arr2));
+// > 1 object, 0 keys
---
+++ 16/readme.md
@@ -0,0 +1,12 @@
+# Class 16: JavaScript Basics
+
+## Required Resources (Pre-requisites)
+* Array methods 4: https://youtu.be/L_6se9m_rrI
+* Handling Objects: https://youtu.be/N0eZ7BXHh2s
+* String methods 1: https://youtu.be/2CwHP49GG60
+
+## Recommended Resources (Optional)
+
+## Syllabus
+
+## Assignment
