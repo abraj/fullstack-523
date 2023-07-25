aec44a3c501c1f455122b766d9bb2a0b5c415a05
Abhishek Raj <abhi@raj.me>
2023-07-25 11:01:45 +0530

    deploy new

---
+++ 08/assign/index.js
@@ -0,0 +1,145 @@
+// Assignment 6.2: Operations on variables
+
+/*
+// Problem 1
+
+// Fill in the blanks with boolean values only, such that the printed output is `true`
+// console.log(... + 1 === ... - ...);
+// > true
+*/
+
+/*
+// Problem 2
+
+// Fill in the three blanks with exactly one of true/false/null, such that the printed output is `3`
+// NOTE: All the three filled values should be different.
+// console.log(2 * ... + 3 * ... - 5 * ...);
+// 3
+*/
+
+/*
+// Problem 3
+
+// Fill in the blank with a value, such that the printed output is `false`
+// console.log(... * 0 === 0);
+// > false
+*/
+
+/*
+// Problem 4
+
+// Fill in each of the blanks with an operator, such that the printed output is correct.
+// const val = '1';
+// console.log(5 + val);
+// console.log(5 + ...val);
+// console.log(5 + ...val);
+// > 51
+// > 6
+// > 5
+*/
+
+/*
+// Problem 5
+
+// Fill in each of the blanks with an operator, such that the printed output is correct.
+// const val = '0';
+// console.log(5 + val);
+// console.log(5 + ...val);
+// console.log(5 + ...val);
+// > 50
+// > 5
+// > 5
+*/
+
+/*
+// Problem 6
+
+// Fill in the blank with some operator(s), such that the printed output is `6`.
+// const val = '0';
+// console.log(5 + ...val);
+// 6
+*/
+
+/*
+// Problem 7
+
+// Fill in the blanks with logical operators (`&&`, `||`), such that the printed output is correct.
+// console.log(null ... 1 ... -5);
+// console.log(null ... 1 ... -5);
+// -5
+// -5
+*/
+
+/*
+// Problem 8
+
+// Fill in the blanks with different `falsy` values, such that the printed output is number `0`.
+// console.log(... + ... + ...);
+// 0
+*/
+
+/*
+// Problem 9
+
+// Fill in the blanks with different `falsy` values, such that the printed output is string `'0'`.
+// console.log(null + false + '');
+// '0'
+*/
+
+/*
+// Problem 10
+
+// Fill in the blank with a value, such that the printed output is `false`.
+// const x = 5 + ...
+// console.log(x === x);
+// > false
+*/
+
+/*
+// Problem 11
+
+// Fill in the blank with a string value, such that the printed output is `true`.
+// const str = ...
+// console.log((5 && str && true) === str);
+// > true
+*/
+
+/*
+// Problem 12
+
+// Fill in the blank with a numeric value, such that the printed output is `true`.
+// const num = ...
+// console.log(('hello' && num && true) === num);
+// > true
+*/
+
+/*
+// Problem 13
+
+// Fill in the blank with a numeric value, such that the printed output is `hello`.
+// const num = ...
+// console.log(false || num && 'hello');
+// 'hello'
+*/
+
+/*
+// Problem 14
+
+// Fill in the blank with a negative number, such that the printed output is `true`.
+// What is the largest possible negative number that can be filled?
+// const num = ...
+// console.log((num || 'hello' && false) === num);
+// > true
+*/
+
+/*
+// Problem 15
+
+// Fill in the blank to create a proper string value, such that the printed output is correct.
+// const a = 91;
+// const b = 'world';
+// const str = ...
+// console.log(str);
+// > Hello world, 
+// > Cloud 91!
+*/
