7c3c13e18c2d09343d409baab25052883072cb99
Abhishek Raj <abhi@raj.me>
2023-07-30 04:29:16 +0530

    Addd assign 15

---
+++ 18/assign/index.js
@@ -0,0 +1,72 @@
+// Assignment 15: String Operations 1
+
+/*******************************/
+// Problem 1:
+
+function multipleSubstring(str, substr) {
+  // ...
+}
+
+// console.log(multipleSubstring('mississippi', 'iss'));
+// > true
+// console.log(multipleSubstring('mississippi', 'is'));
+// > true
+// console.log(multipleSubstring('banana', 'an'));
+// > true
+// console.log(multipleSubstring('banana', 'ana'));
+// > false
+// console.log(multipleSubstring('banana', 'anna'));
+// > false
+
+/*******************************/
+// Problem 2:
+
+function contiguousMultipleSubstring(str, substr) {
+  // ...
+}
+
+// console.log(contiguousMultipleSubstring('mississippi', 'iss'));
+// > true
+// console.log(contiguousMultipleSubstring('mississippi', 'is'));
+// > false
+// console.log(contiguousMultipleSubstring('banana', 'an'));
+// > true
+// console.log(contiguousMultipleSubstring('banana', 'ana'));
+// > false
+// console.log(contiguousMultipleSubstring('banana', 'anna'));
+// > false
+
+/*******************************/
+// Problem 3:
+
+function formatDateTime(dateTimeString) {
+  // ...
+}
+
+// console.log(formatDateTime('2023-07-28T19:08:30.609Z'));
+// > 19:08 28/7/23
+// console.log(formatDateTime('2022-10-28T09:18:30.217Z'));
+// > 9:18 28/10/22
+
+/*******************************/
+// Problem 4:
+
+function formatDateTime2(dateTimeString) {
+  // ...
+}
+
+// console.log(formatDateTime2('2023-07-28T22:08:30.609Z'));
+// > 10:08 PM 28/7/23
+// console.log(formatDateTime2('2022-10-28T09:18:30.217Z'));
+// > 9:18 AM 28/10/22
+
+/*******************************/
+// Problem 5:
+
+function uniqueConstWords(sentence) {
+  // ...
+}
+
+// const sentence = '"To BE or not to be, that is THE question"  -  William Shakespeare';
+// console.log(uniqueConstWords(sentence));
+// > BE, Shakespeare, THE, To
